28665280910ff43b37463f515c8840fe3988def4
adrianaps <tecnologianormal@hotmail.com>
2023-02-07 16:12:09 -0500

    "Add version key"

--- src/db/topic.schema.js
+++ src/db/topic.schema.js
@@ -29,7 +29,8 @@ const topicSchema = new mongoose.Schema({
 {
     toJSON: { virtuals: true }, // So `res.json()` and other `JSON.stringify()` functions include virtuals
     toObject: { virtuals: true }, // So `console.log()` and other functions that use `toObject()` include virtuals
-  }
+    versionKey: false,
+}
 )
 
 const Topic = mongoose.model('Topic', topicSchema);
