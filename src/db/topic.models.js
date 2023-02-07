fd7cbc7e80f8327117be0af93594151f52bd56f7
adrianaps <tecnologianormal@hotmail.com>
2023-02-07 16:03:34 -0500

    "Delete models"

--- src/db/topic.models.js
+++
@@ -1,9 +0,0 @@
-/*const getDB = require('./db')
-
-const getAll = async()=>{
-    const conexion = getDB.getDB()
-    return await conexion.collection("topic").find().toArray()
-}
-
-module.exports = {getAll}*/
-
